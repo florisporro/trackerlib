887adf63931abf89f38c6f566a3aa820be37f988
Floris Porro <floris@streammyevent.com>
2022-09-24 18:20:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import { Route } from "./lib/route";
+import { Tracker } from "./lib/tracker";
+import * as filters from "./lib/filters";
+import * as units from "./lib/units";
 
+export { Route, Tracker, filters, units };

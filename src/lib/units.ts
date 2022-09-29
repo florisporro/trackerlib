1f036746a6ef48596de0484b3f908f6f92c4e6b0
Floris Porro <floris@streammyevent.com>
2022-09-29 19:32:54 +0200

    Add convertDistance

--- src/lib/units.ts
+++ src/lib/units.ts
@@ -9,6 +9,8 @@ export function convertSpeed(speed: Speed, targetUnit:string) {
 		mps = speed.value / 3.6
 	} else if (speed.unit === "m/h") {
 		mps = speed.value / 2.237
+	} else if (speed.unit === "kts") {
+		mps = speed.value / 1.944
 	} else {
 		throw new Error("Invalid speed unit")
 	}
@@ -20,7 +22,23 @@ export function convertSpeed(speed: Speed, targetUnit:string) {
 		return mps * 3.6
 	} else if (targetUnit === "m/h") {
 		return mps * 2.237
+	} else if (targetUnit === "kts") {
+		return mps * 1.944
 	} else {
 		throw new Error("Invalid target speed unit")
+	}
+}
+
+export function convertDistance(distance: number, targetUnit: string) {
+	if (targetUnit === "m") {
+		return distance
+	} else if (targetUnit === "km") {
+		return distance / 1000
+	} else if (targetUnit === "mi") {
+		return distance / 1609.344
+	} else if (targetUnit === "nm") {
+		return distance / 1852
+	} else {
+		throw new Error("Invalid target distance unit")
 	}
 }

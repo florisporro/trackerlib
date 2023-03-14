18d4b1636fb1e146b86aa3a568f97ad59dcc92a2
Floris Porro <floris@streammyevent.com>
2023-03-15 00:03:22 +0100

    Document filters

--- src/lib/filters.ts
+++ src/lib/filters.ts
@@ -1,4 +1,12 @@
-export function lowpassfilter(values: number[], alpha: number) {
+/**
+ * Filters an array of values using a low pass filter
+ *
+ * @export
+ * @param {number[]} values - Array of values to filter
+ * @param {number} alpha - Alpha value for the filter
+ * @return {*}  {number[]} - Filtered array of values
+ */
+export function lowpassfilter(values: number[], alpha: number): number[] {
 	let value = values[0];
 	for (let i = 1; i < values.length; i++){
 			const currentValue = values[i];
